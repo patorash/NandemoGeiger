e841d8e55cbb92fdc41171504f112a89678ab8b9
patorash <chariderpato@gmail.com>
2011-07-30 14:43:46 +0900

    first commit

---
+++ src/ninja/ehime/nangei/NandemoGeigerActivity.java
@@ -0,0 +1,13 @@
+package ninja.ehime.nangei;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NandemoGeigerActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
